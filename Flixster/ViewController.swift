c6bf9a921e20bff8110561c4d0ea3d771070b4c6
Annie <lannie9400@gmail.com>
2020-10-17 11:42:44 -0700

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Yaxin Liu on 10/17/20.
+//  Copyright © 2020 Yaxin Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
