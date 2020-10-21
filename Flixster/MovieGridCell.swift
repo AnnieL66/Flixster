51d67880540636c724aea95d5de6ea42712a8385
Annie <lannie9400@gmail.com>
2020-10-21 15:27:42 -0700

    Assignment #2

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Yaxin Liu on 10/19/20.
+//  Copyright © 2020 Yaxin Liu. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
